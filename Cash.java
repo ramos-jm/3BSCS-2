cc9a15aa09df836481b7f5de4e3934ee1db04a6a
John Michael Calitis Ramos <127398189+ramos-jm@users.noreply.github.com>
2024-02-08 00:08:21 +0000

    3BSCS-2 Strategy Pattern

---
+++ Cash.java
@@ -0,0 +1,9 @@
+public class Cash implements PaymentType{
+    public String calculatePayment(){
+        String output = new String();
+        output += "Payment is Cash";
+        output += "No incentives";
+
+        return output;
+    }
+}
---
+++ GCash.java
@@ -0,0 +1,9 @@
+public class GCash implements PaymentType{
+    public String calculatePayment(){
+        String output = new String();
+        output += "Payment is Gcash\n";
+        output += "Energy for Trees: 10 points";
+
+        return output;
+    }
+}
